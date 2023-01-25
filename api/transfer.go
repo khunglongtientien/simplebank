9c46596f3670934b9a69316450be82f11f092821
NguyenVanBac <khunglongtientien@gmail.com>
2023-01-25 20:52:06 +0700

    add custom validator

---
+++ api/transfer.go
@@ -0,0 +1,66 @@
+package api
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	db "simplebank/db/sqlc"
+)
+
+type transferRequest struct {
+	FromAccountId int64  `json:"from_account_id" binding:"required,min=1"`
+	ToAccountId   int64  `json:"to_account_id" binding:"required,min=1"`
+	Amount        int64  `json:"amount" binding:"required,gt=0"`
+	Currency      string `json:"currency" binding:"required,currency"`
+}
+
+func (s *Server) createTransfer(c *gin.Context) {
+	var req transferRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	if !s.validAccount(c, req.FromAccountId, req.Currency) {
+		return
+	}
+
+	if !s.validAccount(c, req.ToAccountId, req.Currency) {
+		return
+	}
+
+	arg := db.TransferTxParams{
+		FromAccountID: req.FromAccountId,
+		ToAccountID:   req.ToAccountId,
+		Amount:        req.Amount,
+	}
+
+	transfer, err := s.store.TransferTx(c, arg)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	c.JSON(http.StatusOK, transfer)
+}
+
+func (s *Server) validAccount(c *gin.Context, accountID int64, currency string) bool {
+	account, err := s.store.GetAccount(c, accountID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, errorResponse(err))
+			return false
+		}
+		c.JSON(http.StatusInternalServerError, errorResponse(err))
+		return false
+	}
+
+	if account.Currency != currency {
+		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", accountID, account.Currency, currency)
+		c.JSON(http.StatusBadRequest, errorResponse(err))
+		return false
+	}
+
+	return true
+}
