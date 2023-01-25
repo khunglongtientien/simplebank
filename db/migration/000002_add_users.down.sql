a0cd9516b7e3e4461ba0e1594a2d8584c98bdcea
NguyenVanBac <khunglongtientien@gmail.com>
2023-01-25 21:11:28 +0700

    add user table

---
+++ db/migration/000002_add_users.down.sql
@@ -0,0 +1,5 @@
+alter table if exists "accounts" drop constraint if exists "accounts_owner_fkey";
+
+alter table if exists "accounts" drop constraint if exists "owner_currency_key";
+
+drop table if exists "users"
